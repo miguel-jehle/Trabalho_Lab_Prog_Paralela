6ed667e9800f19544efbafc0d73ee44598553855
Miguel Jehle <miguelbjh@id.uff.br>
2026-06-01 17:06:37 -0300

    Nense

---
+++ BOT_static.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <omp.h>
+
+int primo (long int n) {
+	int result = 1;
+	#pragma omp parallel for shared(result) schedule(static, 10)
+	for (long int i = 3; i < (long int)(sqrt(n) + 1); i+=2){
+	     if (n%i == 0) {
+	     	result = 0;
+         }
+    }	         
+	return result;
+}
+
+int main(int argc, char *argv[]) { /* primos_seq.c  */
+double t_inicio, t_fim;
+long int  n, total=0;
+
+    if (argc < 2) {
+        printf("Valor inválido! Entre com o valor do maior inteiro\n");
+       	return 0;
+    } else {
+        n = strtol(argv[1], (char **) NULL, 10);
+    }
+        
+    total++; /* Adiciona 2 que é primo */
+    
+    t_inicio = omp_get_wtime();
+    
+    #pragma omp parallel for reduction(+:total) schedule(dynamic, 10) num_threads(4)
+        for (long int i = 3; i <= n; i += 2) {
+            if(primo(i) == 1) total++;
+        }
+    
+    t_fim = omp_get_wtime();
+    printf("Quant. de primos entre 1 e %ld: %ld \n", n, total);
+    printf("Tempo de execução: %3.10f\n", t_fim-t_inicio);
+    
+    return(0);
+}
+
